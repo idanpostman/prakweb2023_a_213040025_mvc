9a7b7ca91603fd60a484d8a9e51a93ac8044c45d
idanpostman <idanpostman@gmail.com>
2023-10-20 19:06:41 +0700

    Push Awal

---
+++ 01. Pendahuluan/pendahuluan.php
@@ -0,0 +1,23 @@
+<?php 
+/* 
+MVC = Model - View - Controller
+"Pola Arsitektur pada perancangan perangkat lunak berorientasi objek"
+Memisahkan antara tampilan, data dan proses
+
+Model (Data): Representasi pengetahuan, Mengelola data, Logika bisnis
+controller (Proses): Perantara antara model dan view, Menangani pemrosesan pada aplikasi, menangani aksi dari uase
+view (Tampilan) : Output, apa yang dilihat oleh user, representasi visual dari model, lapisan presentasi
+
+Kenapa MVC?
+- Organisasi dan Struktur kode
+- Pemisahan logic dan tampilan
+- Perawatan kode
+- Implementasi konsep OOP yang sudah dipelajari
+- Digunakan oleh banyak Web Application Framework
+
+FRAMEWORK MVC PADA PHP
+CodeIgneiter, Laravel, Yii, dll
+
+*/
+
+?>
